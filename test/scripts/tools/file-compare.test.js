6846e07e39e5b1ac6fe5dcb81796e4648c9f56cb
helinjiang <me@helinjiang.com>
2018-06-28 01:00:59 +0800

    feat: 增加 tools/file-compare.js 的测试用例

--- test/scripts/tools/file-compare.test.js
+++ test/scripts/tools/file-compare.test.js
@@ -16,6 +16,18 @@ describe('校验 tools/file-compare.js', function () {
       expect(compareResult).to.be.an('object').and.have.all.keys('onlyInA', 'onlyInB', 'both');
     });
 
+    it('有两个文件只存在于a中', function () {
+      expect(compareResult).to.have.property('onlyInA')
+        .that.is.an('array')
+        .that.to.have.lengthOf(2);
+    });
+
+    it('只存在于a中的文件分别为 compare-2-different-md5.txt 和 compare-4.txt', function () {
+      expect(compareResult['onlyInA'].map(function (item) {
+        return item.relativePath;
+      })).to.have.same.members(['compare-2-different-md5.txt', 'compare-4.txt']);
+    });
+
     it('有两个文件只存在于b中', function () {
       expect(compareResult).to.have.property('onlyInB')
         .that.is.an('array')
@@ -28,18 +40,18 @@ describe('校验 tools/file-compare.js', function () {
       })).to.have.same.members(['compare-2-different-md5.txt', 'compare-5.txt', 'subdir/compare-6.txt']);
     });
 
-    it('文件比较之后：有两个文件同时存在a和b中', function () {
+    it('有两个文件同时存在a和b中', function () {
       expect(compareResult).to.have.property('both')
         .that.is.an('object')
         .that.to.include.keys('d6449191e61e5d22913d261d1938ca9b')
         .that.to.include.keys('e820a3de6b926b0bbe84b781f3aee457');
     });
 
-    it('文件比较之后：d6449191e61e5d22913d261d1938ca9b 对应三条记录', function () {
+    it('d6449191e61e5d22913d261d1938ca9b 对应两条记录', function () {
       expect(compareResult.both['d6449191e61e5d22913d261d1938ca9b']).to.have.lengthOf(2);
     });
 
-    it('文件比较之后：e820a3de6b926b0bbe84b781f3aee457 对应两条记录', function () {
+    it('e820a3de6b926b0bbe84b781f3aee457 对应四条记录', function () {
       expect(compareResult.both['e820a3de6b926b0bbe84b781f3aee457']).to.have.lengthOf(4);
     });
 
