0189f049ba5f9a147d8b856db20c924c24f644e8
helinjiang <me@helinjiang.com>
2019-02-03 10:45:31 +0800

    feat: 分析数据

--- demo/a-scan-01.js
+++ demo/a-scan-01.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const data = require('../tmp/scan_data');
 
 // console.log(Object.keys(data))
@@ -7,5 +8,20 @@ const data = require('../tmp/scan_data');
 console.log(data.basePath)
 data.multiArr.forEach((md5) => {
     console.log('\n-----------------------------')
-    console.log(data.map[md5])
+    // console.log(data.map[md5])
+    let list = [];
+    data.map[md5].forEach((fileItem) => {
+        // console.log(fileItem.mtime, fileItem.relativePath)
+        let fullPath = path.join(data.basePath, fileItem.relativePath)
+        console.log(fullPath)
+
+        if (list.indexOf(fileItem.mtime) < 0) {
+            list.push(fileItem.mtime)
+        }
+    })
+
+    if(list.length>1){
+        console.log(list)
+    }
+
 })
